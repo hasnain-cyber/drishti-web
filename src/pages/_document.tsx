4014209bec2d17bfbdbdcf71cfc043d3c55437e7
Abhinav Yadav <actcrick02@gmail.com>
2023-02-10 22:06:23 +0530

    add style variables

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,13 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link rel="stylesheet" href="https://kit.fontawesome.com/d48aa7277c.css" crossOrigin="anonymous" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
+        <link href="https://fonts.googleapis.com/css2?family=Libre+Franklin:ital,wght@1,900&family=Montserrat:ital,wght@1,100;1,200&family=Rubik&display=swap" rel="stylesheet"></link>
+        <script src="https://kit.fontawesome.com/d48aa7277c.js" crossOrigin="anonymous"></script>
+      </Head>
       <body>
         <Main />
         <NextScript />
