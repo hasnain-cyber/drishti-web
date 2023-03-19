5d08a459b0a18958b8270254b48a7b0f8b90635d
Abhinav Yadav <actcrick02@gmail.com>
2023-03-20 01:12:01 +0530

    add profile page

--- src/pages/users/[id]/edit.tsx
+++ src/pages/users/[id]/edit.tsx
@@ -59,7 +59,71 @@ const edit = () => {
                         Go Back
                     </div>
                 </div>
+                
+                {/* Make a main container to display the content of the selected tab */}
+                <div className={`${styles.edit__main}`}>
+                    <div className={`${styles.edit__main__container}`} id="profile_tab">
+                        <h1>Profile</h1>
+                        <p>Update Your Profile Information</p>
+                        <div className={`${styles.profile__picture}`}>
+                            <img src="/assets/profile/dp.jpg" alt="profile" className={`${styles.dp__image}`} />
+                            <div className={`${styles.edit__dp}`} >
+                                <Button className={`${styles.upload__dp}`}>Upload Image</Button>
+                                <Button className={`${styles.remove__dp}`}><i className="fa-solid fa-trash" />Remove</Button>
+                            </div>
+                        </div>
+                        <div className={`${styles.container__form}`}>
+                            <Form>
+                                <Form.Group className="mb-3" controlId="formBasicEmail">
+                                    <Form.Label>Email Address</Form.Label>
+                                    <Form.Control type="email" placeholder="anubhav.singh@iiti.ac.in" disabled className={`${styles.text__input}`} />
+                                </Form.Group>
 
+                                <Form.Group className="mb-3" controlId="formBasicName">
+                                    <Form.Label>Name</Form.Label>
+                                    <Form.Control type="text" placeholder="Anubhav Singh" className={`${styles.text__input}`} />
+                                    <Form.Text className="text-muted m-1">
+                                        Please Include Your Full Name along with the Title (Dr./Proff.)
+                                    </Form.Text>
+                                </Form.Group>
+
+                                {/* Department form */}
+                                <Form.Group className="mb-3" controlId="formBasicDepartment">
+                                    <Form.Label>Department</Form.Label>
+                                    <Form.Control type="text" placeholder="Department of Computer Science and Engineering" className={`${styles.text__input}`} />
+                                </Form.Group>
+
+                                {/* Institute Form */}
+                                <Form.Group className="mb-3" controlId="formBasicInstitute">
+                                    <Form.Label>Institute</Form.Label>
+                                    <Form.Control type="text" placeholder="Indian Institute of Technology Indore" className={`${styles.text__input}`} />
+                                </Form.Group>
+
+                                {/* Contact Number */}
+                                <Form.Group className="mb-3" controlId="formBasicContact">
+                                    <Form.Label>Contact Number</Form.Label>
+                                    <Form.Control type="text" placeholder="+91 9234567890" className={`${styles.text__input}`} />
+                                </Form.Group>
+
+                                {/* Linked In Form */}
+                                <Form.Group className="mb-3" controlId="formBasicLinkedIn">
+                                    <Form.Label>LinkedIn</Form.Label>
+                                    <div className={`${styles.input__group}`}>
+                                        <h3>Text to Display:</h3>
+                                        <Form.Control type="text" placeholder="Anubhav Singh Bassi" className={`${styles.text__input}`} />
+                                        <h3>URL:</h3>
+                                        <Form.Control type="text" placeholder="https://www.linkedin.com/in/anubhav-singh-bassi-0b1b3b1b3/" className={`${styles.text__input}`} />
+                                    </div>
+                                </Form.Group>
+
+                                <Button variant="primary" type="submit" className={`mt-4 ${styles.upload__dp}`}>
+                                    Submit
+                                </Button>
+                            </Form>
+                        </div>
+                    </div>
+                    
+                </div>
             </div>
         </div>
     );
